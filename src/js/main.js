003db2eb29a0861e95e168f6fa0a4e6ab207832e
Axel Tavormina <tavormina.axel@gmail.com>
2019-05-20 16:46:41 +0200

    feat(Nav): smooth scroll

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ function init() {
   const header = document.querySelector('.header');
   const nav = document.querySelector('.nav');
   const navLinks = document.querySelectorAll('.nav .link');
+  const sectionLinks = document.querySelectorAll('.section-link');
   const btnOpenMenu = document.querySelector('.btn-open-menu');
   const btnCloseMenu = document.querySelector('.btn-close-menu');
   const hero = document.querySelector('.hero');
@@ -18,7 +19,8 @@ function init() {
   btnOpenMenu.addEventListener('click', openMenu);
   btnCloseMenu.addEventListener('click', closeMenu);
   navLinks.forEach(navLink => navLink.addEventListener('click', closeMenu));
-  btn;
+  navLinks.forEach(link => link.addEventListener('click', smoothScroll));
+  sectionLinks.forEach(link => link.addEventListener('click', smoothScroll));
 
   // Animate Header when scrolling
   function animateHeader() {
@@ -31,11 +33,24 @@ function init() {
     }
   }
 
+  // Handle menu
   function openMenu() {
     nav.classList.add('open');
   }
 
   function closeMenu() {
     nav.classList.remove('open');
+  }
+
+  // Add smooth scroll when cliking on nav link
+  function smoothScroll(e) {
+    console.log('test');
+    const section = document.querySelector(this.hash);
+    e.preventDefault();
+
+    window.scrollTo({
+      top: section.offsetTop - headerHeight,
+      behavior: 'smooth'
+    });
   }
 }
