2bba95a4b1b2989f45be98b53e67138196bcac95
Axel Tavormina <tavormina.axel@gmail.com>
2019-05-20 16:21:16 +0200

    feat(Nav): open & close menu

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,41 @@
-const header = document.querySelector('.header');
-const hero = document.querySelector('.hero');
-const heroHeight = hero.offsetHeight;
-const headerHeight = header.offsetHeight;
+addEventListener('DOMContentLoaded', init);
 
-// Listeners
-addEventListener('scroll', () => {
-  animateHeader();
-});
+function init() {
+  const header = document.querySelector('.header');
+  const nav = document.querySelector('.nav');
+  const navLinks = document.querySelectorAll('.nav .link');
+  const btnOpenMenu = document.querySelector('.btn-open-menu');
+  const btnCloseMenu = document.querySelector('.btn-close-menu');
+  const hero = document.querySelector('.hero');
+  const heroHeight = hero.offsetHeight;
+  const headerHeight = header.offsetHeight;
 
-// Animate Header when scrolling
-function animateHeader() {
-  const scrollY = window.scrollY;
+  // Listeners
+  addEventListener('scroll', () => {
+    animateHeader();
+  });
 
-  if (scrollY > heroHeight - headerHeight) {
-    header.classList.add('header-animate');
-  } else {
-    header.classList.remove('header-animate');
+  btnOpenMenu.addEventListener('click', openMenu);
+  btnCloseMenu.addEventListener('click', closeMenu);
+  navLinks.forEach(navLink => navLink.addEventListener('click', closeMenu));
+  btn;
+
+  // Animate Header when scrolling
+  function animateHeader() {
+    const scrollY = window.scrollY;
+
+    if (scrollY > heroHeight - headerHeight) {
+      header.classList.add('header-animate');
+    } else {
+      header.classList.remove('header-animate');
+    }
+  }
+
+  function openMenu() {
+    nav.classList.add('open');
+  }
+
+  function closeMenu() {
+    nav.classList.remove('open');
   }
 }
